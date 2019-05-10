51576ad22a246cf148c3a47f428beb147d2cbaa2
tingwei-fly <michellemax3691@gmail.com>
2019-05-10 10:14:11 +0800

    backstage function is ok

--- routes/admin.js
+++ routes/admin.js
@@ -17,7 +17,6 @@ router
 	//original path: req._parsedOriginalUrl.pathname
 	if (req.session.loggedin) {
 	    next();
-        //res.redirect('/admin/index');
 	} else {
 	    res.redirect('/login');
 	}
@@ -26,10 +25,28 @@ router
     .get('/index', function(reg, res){
     res.render('admin/index', {title: 'Admin home'});
     })
+/////////////////////Events/////////////////////
+    .get('/events', function(req, res){
+	var db = req.con;
+	db.query('SELECT * FROM Events', function(err, data) {
+            if (err) console.log(err);
+	    res.render('admin/events', { title: 'Admin Event', Events: data});
+	});
+    })
+
+    .post('/events/delete', function(req, res){
+	var db = req.con;
+	var del = req.body.del;
+	db.query("DELETE FROM Events WHERE EventID = ?", del, function(err) {
+	    if (err) console.log(err);
+	});
+	res.redirect('/admin/events');
+    })
 
     .get('/events/add', function(req, res){
 	res.render('admin/add/events', { title: 'Add Events'});
     })
+
     .post('/events/add', function(req, res){
 	var db = req.con;
 	db.query('SELECT * FROM Events', function(err, data) {
@@ -39,6 +56,7 @@ router
 	    if (data.length != 0) id = data[data.length-1].EventID + 1;
 	    var sql = {
 		EventID: id,
+        Category: req.body.Category,
 		EventName: req.body.EventName,
 		CreateDate: date,
 		Content: req.body.Content
@@ -46,18 +64,37 @@ router
 	    db.query('INSERT INTO Events SET ?', sql, function(err, rows) {
 		if (err) console.log(err);
 		res.setHeader('Content-Type', 'application/json');
-		res.redirect('/events');
+		res.redirect('/admin/events');
 	    });
 	});
     })
 
     .get('/events/:id', function(req, res){
-	res.send('events get' + req.params.id);
-    })
-    .post('/events/:id', function(req, res){
-	res.send('events post' + req.params.id);
+	var db = req.con;
+	db.query("SELECT * FROM Events WHERE EventID = ?", req.params.id, function(err, data) {
+            if (err) console.log(err);
+	    res.render('admin/modify/events', { title: 'Modify Event', Events: data[0] });
+	});
     })
 
+    .post('/events/modify', function(req, res){
+	var db = req.con;
+    var date = new Date().toISOString().slice(0, 19).replace('T', ' ');;
+	var sql = {
+	    EventID: req.body.EventID,
+        Category: req.body.Category,
+		EventName: req.body.EventName,
+		CreateDate: date,
+		Content: req.body.Content
+	};
+	db.query('UPDATE Events SET ? WHERE EventID = ?', [sql, sql.EventID], function(err, rows) {
+	    if (err) {console.log(err); return;}
+	    res.setHeader('Content-Type', 'application/json');
+	    res.redirect('/admin/events');
+	});
+    })
+
+/////////////////////Projects/////////////////////
     .get('/projects', function(req, res){
 	var db = req.con;
 	db.query('SELECT * FROM Projects', function(err, data) {
@@ -65,6 +102,7 @@ router
 	    res.render('admin/projects', { title: 'Admin Project', Projects: data});
 	});
     })
+
     .post('/projects/delete', function(req, res){
 	var db = req.con;
 	var del = req.body.del;
@@ -73,9 +111,11 @@ router
 	});
 	res.redirect('/admin/projects');
     })
+
     .get('/projects/add', function(req, res){
 	res.render('admin/add/projects', { title: 'Add Project'});
     })
+
     .post('/projects/add', function(req, res){
 	var db = req.con;
 	db.query('SELECT * FROM Projects', function(err, data) {
@@ -90,7 +130,7 @@ router
 	    db.query('INSERT INTO Projects SET ?', sql, function(err, rows) {
 		if (err) console.log(err);
 		res.setHeader('Content-Type', 'application/json');
-		res.redirect('/projects');
+		res.redirect('/admin/projects');
 	    });
 	});
     })
@@ -102,6 +142,7 @@ router
 	    res.render('admin/modify/projects', { title: 'Modify Project', Projects: data[0] });
 	});
     })
+
     .post('/projects/modify', function(req, res){
 	var db = req.con;
 	var sql = {
@@ -115,6 +156,23 @@ router
 	    res.redirect('/admin/projects');
 	});
     })
+/////////////////////Albums/////////////////////
+    .get('/albums', function(req, res){
+	var db = req.con;
+	db.query('SELECT * FROM Albums', function(err, data) {
+            if (err) console.log(err);
+	    res.render('admin/albums', { title: 'Admin Album', Albums: data});
+	});
+    })
+
+    .post('/albums/delete', function(req, res){
+	var db = req.con;
+	var del = req.body.del;
+	db.query("DELETE FROM Albums WHERE AlbumID = ?", del, function(err) {
+	    if (err) console.log(err);
+	});
+	res.redirect('/admin/albums');
+    })
 
     .get('/albums/add', function(req, res){ 
 	res.render('admin/add/albums', { title: 'Add Albums'});
@@ -124,19 +182,48 @@ router
 	db.query('SELECT * FROM Albums', function(err, data) {
             if (err) console.log(err);
 	    var id = 1;
+        var date = new Date().toISOString().slice(0, 19).replace('T', ' ');;
 	    if (data.length != 0) id = data[data.length-1].AlbumID + 1;
 	    var sql = {
 		AlbumID: id,
+        Creator: req.body.Creator,
 		AlbumName: req.body.AlbumName,
 		URL: req.body.URL,
-		CoverImg: req.file.filename
+        CreateDate: date,
+		//CoverImg: req.file.filename
 	    };
 	    db.query('INSERT INTO Albums SET ?', sql, function(err, rows) {
 		if (err) console.log(err);
 		res.setHeader('Content-Type', 'application/json');
-		res.redirect('/albums');
+		res.redirect('/admin/albums');
 	    });
 	});
-    });
+    })
+
+    .get('/albums/:id', function(req, res){
+	var db = req.con;
+	db.query("SELECT * FROM Albums WHERE AlbumID = ?", req.params.id, function(err, data) {
+            if (err) console.log(err);
+	    res.render('admin/modify/albums', { title: 'Modify Album', Albums: data[0] });
+	});
+    })
+
+    .post('/albums/modify', function(req, res){
+	var db = req.con;
+    var date = new Date().toISOString().slice(0, 19).replace('T', ' ');;
+	var sql = {
+	    AlbumID: req.body.AlbumID,
+        Creator: req.body.Creator,
+		AlbumName: req.body.AlbumName,
+		URL: req.body.URL,
+        CreateDate: date,
+		//CoverImg: req.file.filename
+	};
+	db.query('UPDATE Albums SET ? WHERE AlbumID = ?', [sql, sql.AlbumID], function(err, rows) {
+	    if (err) {console.log(err); return;}
+	    res.setHeader('Content-Type', 'application/json');
+	    res.redirect('/admin/albums');
+	});
+    })
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,11 @@ var path = require('path');
 router
     .get('/', function(req, res) {
 	var db = req.con;
-	var title = 'Projects Test';
+	var title = 'NTU CSIE WEB';
 	var projects, events;
-	db.query('SELECT * FROM Projects ORDER BY ProjectID DESC LIMIT 10; SELECT * FROM Events ORDER BY EventID DESC LIMIT 10;', function(err, data) {
+	db.query('SELECT * FROM Projects ORDER BY ProjectID DESC LIMIT 10; SELECT * FROM Events ORDER BY EventID DESC LIMIT 10; SELECT COUNT(1) AS count FROM Projects; SELECT COUNT(1) AS count FROM Events;', function(err, data) {
         if (err) {console.log(err); return; }
-	    res.render('index', { title: title, Projects: data[0], Events: data[1]});
+	    res.render('index', { title: title, Projects: data[0], Events: data[1], Projects_TotalPage: data[2], Events_TotalPage: data[3]});
 	});
     })
     .get('/login', function(req, res) {
@@ -38,6 +38,7 @@ router
 	    res.end();
 	}
     })
+
     .get('/projects', function(req, res, next) {
 	var db = req.con;
 	var title = 'Projects Test';
@@ -61,34 +62,60 @@ router
             if (err) {console.log(err); return; }
 	    res.render('albums', { title: title, Albums: albums});
 	});
-    });
+    })
 
-var bodyParser = require('body-parser');
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+    .get('/event_content/:id', function(req, res){
+	var db = req.con;
+	db.query("SELECT * FROM Events WHERE EventID = ?", req.params.id, function(err, data) {
+            if (err) console.log(err);
+	    res.render('event_content', { title: 'More about Event', Events: data[0] });
+	});
+    })
 
-/*
-router.get('/ajax', function(req, res, next) {
-  res.render('ajax');
-});
-*/
+    .get('/project_content/:id', function(req, res){
+	var db = req.con;
+	db.query("SELECT * FROM Projects WHERE ProjectID = ?", req.params.id, function(err, data) {
+            if (err) console.log(err);
+	    res.render('project_content', { title: 'More about Project', Projects: data[0] });
+	});
+    })
 
-router.post('/req_ajax', urlencodedParser, function(req, res, next) {
+    .post('/events_ajax', function(req, res, next) {
     var skip = req.body.pageNum;
     var num = parseInt(skip)*10;
     
     var db = req.con;
     //db.connect(function(err){if (err) throw err;});
-    var Q = "SELECT EventName, Category, CreateDate FROM Events LIMIT " + num + ",10"; 
+    var Q = "SELECT EventID, EventName, Category, CreateDate FROM Events ORDER BY EventID DESC LIMIT " + num +", 10"; 
     var objson = [];
     db.query(Q, function (err, result, fields) {
         if (err) throw err;
         for (var i = 0 ; i < result.length;i++) {
-            objson.push({EventName:result[i].EventName, Category:result[i].Category, CreateDate:result[i].CreateDate});
+            objson.push({
+                EventID:result[i].EventID,
+                EventName:result[i].EventName, Category:result[i].Category, CreateDate:result[i].CreateDate});
         }
         res.send(JSON.stringify(objson));
     });
+    })
+
+    .post('/projects_ajax', function(req, res, next) {
+    var skip = req.body.pageNum;
+    var num = parseInt(skip)*10;
     
- });
+    var db = req.con;
+    //db.connect(function(err){if (err) throw err;});
+    var Q = "SELECT ProjectID, LabName, Description FROM Projects ORDER BY ProjectID DESC LIMIT " + num +", 10"; 
+    var objson = [];
+    db.query(Q, function (err, result, fields) {
+        if (err) throw err;
+        for (var i = 0 ; i < result.length;i++) {
+            objson.push({
+                ProjectID:result[i].ProjectID, LabName:result[i].LabName, Description:result[i].Description});
+        }
+        res.send(JSON.stringify(objson));
+    });
+    })
 
 
 module.exports = router;
