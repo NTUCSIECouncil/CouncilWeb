1d9f5bfbd2381355bbf10603335986f7f0ae5fba
HsiaoIJen <s123unny@gmail.com>
2019-07-03 22:21:48 +0800

    fix no-end next page

--- public/Javascripts/ajax.js
+++ public/Javascripts/ajax.js
@@ -3,18 +3,30 @@ var events_curPage = 1, projects_curPage = 1;
 /*Events*/
 function events_getData(page)
 {
-    $.ajax({
+    return $.ajax({
         type: 'POST',
         url: "/events_ajax",
         data: {'pageNum':page},
         dataType: 'json',
         
         success: function(json){
-            $("#events_tbody").empty();
             var row = "";
             var list = json;
+            if (list.length == 0) {
+                return;
+            }
+            $("#events_tbody").empty();
             $.each(list,function(index,array){
-                row += "<tr><td align='center' valign='center' class='col_1'>" + array['Category'] + "</td><td align='center' valign='center' class='col_2'><a href='/event_content/" + array['EventID'] +"'>"+ array['EventName'] + "</a></td><td align='center' valign='center' class='col_3'>" + array['CreateDate'] + "</td></tr>";
+                row += "<tr> \
+                    <td align='center' valign='center' class='col_1'>" 
+                        + array['Category'] + 
+                    "</td> \
+                    <td align='center' valign='center' class='col_2'> \
+                        <a href='/event_content/" + array['EventID'] + "'>" + array['EventName'] + "</a> \
+                    </td> \
+                    <td align='center' valign='center' class='col_3'>"
+                        + array['CreateDate'] + 
+                    "</td></tr>";
             });
             $("#events_tbody").append(row);
         },
@@ -37,32 +49,41 @@ function events_previouspage()
 {
     if (events_curPage > 1) {
         var i = events_curPage-2;
-        events_getData(i);
-        events_curPage--;
-        document.getElementById("events_curPage").innerHTML = "目前頁數："+events_curPage;
+        events_getData(i).done(function(res) {
+            if (res.length > 0) {
+                events_curPage--;
+                document.getElementById("events_curPage").innerHTML = "目前頁數："+events_curPage;
+            }
+        });
     }
 }
 
 function events_nextpage()
 {
-    events_getData(events_curPage);
-    events_curPage++;
-    document.getElementById("events_curPage").innerHTML = "目前頁數："+events_curPage;
+    events_getData(events_curPage).done(function(res) {
+        if (res.length > 0) {
+            events_curPage++;
+            document.getElementById("events_curPage").innerHTML = "目前頁數："+events_curPage;
+        }
+    });
 }
 
 /*Projects*/
 function projects_getData(page)
 {
-    $.ajax({
+    return $.ajax({
         type: 'POST',
         url: "/projects_ajax",
         data: {'pageNum':page},
         dataType: 'json',
         
         success: function(json){
-            $("#projects_tbody").empty();
             var row = "";
             var list = json;
+            if (list.length == 0) {
+                return;
+            }
+            $("#projects_tbody").empty();
             $.each(list,function(index,array){
                 row += "<tr><td align='center' valign='center' class='col_1'>" + array['ProjectID'] + "</td><td align='center' valign='center' class='col_2'><a href='/project_content/"+ array['ProjectID'] +"'>"+ array['LabName'] + "</a></td><td align='center' valign='center' class='col_3'>" + array['Description'] + "</td></tr>";
             });
@@ -87,15 +108,21 @@ function projects_previouspage()
 {
     if (projects_curPage > 1) {
         var i = projects_curPage-2;
-        projects_getData(i);
-        projects_curPage--;
-        document.getElementById("projects_curPage").innerHTML = "目前頁數："+projects_curPage;
+        projects_getData(i).done(function(res) {
+            if (res.length > 0) {
+                projects_curPage--;
+                document.getElementById("projects_curPage").innerHTML = "目前頁數："+projects_curPage;
+            }
+        });
     }
 }
 
 function projects_nextpage()
 {
-    projects_getData(projects_curPage);
-    projects_curPage++;
-    document.getElementById("projects_curPage").innerHTML = "目前頁數："+projects_curPage;
+    projects_getData(projects_curPage).done(function(res) {
+        if (res.length > 0) {
+            projects_curPage++;
+            document.getElementById("projects_curPage").innerHTML = "目前頁數："+projects_curPage;
+        }
+    });
 }
--- routes/index.js
+++ routes/index.js
@@ -6,115 +6,101 @@ var path = require('path');
 //render: ../../views/ejs
 router
     .get('/', function(req, res) {
-	var db = req.con;
-	var title = 'NTU CSIE WEB';
-	var projects, events;
-	db.query('SELECT * FROM Projects ORDER BY ProjectID DESC LIMIT 10; SELECT * FROM Events ORDER BY EventID DESC LIMIT 10; SELECT COUNT(1) AS count FROM Projects; SELECT COUNT(1) AS count FROM Events;', function(err, data) {
-        if (err) {console.log(err); return; }
-	    res.render('index', { title: title, Projects: data[0], Events: data[1], Projects_TotalPage: data[2], Events_TotalPage: data[3]});
-	});
+    	var db = req.con;
+    	var title = 'NTU CSIE WEB';
+    	var projects, events;
+    	db.query('SELECT * FROM Projects ORDER BY ProjectID DESC LIMIT 10; SELECT * FROM Events ORDER BY EventID DESC LIMIT 10; SELECT COUNT(1) AS count FROM Projects; SELECT COUNT(1) AS count FROM Events;', function(err, data) {
+            if (err) { console.log(err); return; }
+            console.log(data[1][0]);
+    	    res.render('index', { 
+                title: title, Projects: data[0], Events: data[1], Projects_TotalPage: data[2], Events_TotalPage: data[3]
+            });
+    	});
     })
     .get('/login', function(req, res) {
-	res.render('login');
+    	res.render('login');
     })
     .post('/auth', function(req, res) {
-	var db = req.con;
-	var username = req.body.username;
-	var password = req.body.password;
-	if (username && password) {
-	    db.query('SELECT * FROM Accounts WHERE username = ? AND password = ?',
-			     [username, password], function(err, results, fields) {
-		if (results.length > 0) {
-		    req.session.loggedin = true;
-		    req.session.username = username;
-		    res.redirect('/admin/index');
-		} else {
-		    res.send('Incorrect Username and/or Password!');
-		}			
-		res.end();
-	    });
-	} else {
-	    res.send('Please enter Username and Password!');
-	    res.end();
-	}
+    	var db = req.con;
+    	var username = req.body.username;
+    	var password = req.body.password;
+    	if (username && password) {
+    	    db.query('SELECT * FROM Accounts WHERE username = ? AND password = ?',
+    			     [username, password], function(err, results, fields) {
+        		if (results.length > 0) {
+        		    req.session.loggedin = true;
+        		    req.session.username = username;
+        		    res.redirect('/admin/index');
+        		} else {
+        		    res.send('Incorrect Username and/or Password!');
+        		}			
+        		res.end();
+    	    });
+    	} else {
+    	    res.send('Please enter Username and Password!');
+    	    res.end();
+    	}
     })
 
-    .get('/projects', function(req, res, next) {
-	var db = req.con;
-	var title = 'Projects Test';
-	db.query('SELECT * FROM Projects', function(err, projects) {
-            if (err) {console.log(err); return; }
-	    res.render('projects', { title: title, Projects: projects});
-	});
-    })
-    .get('/events', function(req, res, next) {
-	var db = req.con;
-	var title = 'Events Test';
-	db.query('SELECT * FROM Events', function(err, events) {
-            if (err) {console.log(err); return; }
-	    res.render('events', { title: title, Events: events});
-	});
-    })
     .get('/albums', function(req, res, next) {
-	var db = req.con;
-	var title = 'Albums Test';
-	db.query('SELECT * FROM Albums', function(err, albums) {
-            if (err) {console.log(err); return; }
-	    res.render('albums', { title: title, Albums: albums});
-	});
+    	var db = req.con;
+    	var title = 'Albums Test';
+    	db.query('SELECT * FROM Albums', function(err, albums) {
+                if (err) {console.log(err); return; }
+    	    res.render('albums', { title: title, Albums: albums});
+    	});
     })
 
     .get('/event_content/:id', function(req, res){
-	var db = req.con;
-	db.query("SELECT * FROM Events WHERE EventID = ?", req.params.id, function(err, data) {
-            if (err) console.log(err);
-	    res.render('event_content', { title: 'More about Event', Events: data[0] });
-	});
+    	var db = req.con;
+    	db.query("SELECT * FROM Events WHERE EventID = ?", req.params.id, function(err, data) {
+                if (err) console.log(err);
+    	    res.render('event_content', { title: 'More about Event', Events: data[0] });
+    	});
     })
 
     .get('/project_content/:id', function(req, res){
-	var db = req.con;
-	db.query("SELECT * FROM Projects WHERE ProjectID = ?", req.params.id, function(err, data) {
-            if (err) console.log(err);
-	    res.render('project_content', { title: 'More about Project', Projects: data[0] });
-	});
+    	var db = req.con;
+    	db.query("SELECT * FROM Projects WHERE ProjectID = ?", req.params.id, function(err, data) {
+                if (err) console.log(err);
+    	    res.render('project_content', { title: 'More about Project', Projects: data[0] });
+    	});
     })
 
     .post('/events_ajax', function(req, res, next) {
-    var skip = req.body.pageNum;
-    var num = parseInt(skip)*10;
-    
-    var db = req.con;
-    //db.connect(function(err){if (err) throw err;});
-    var Q = "SELECT EventID, EventName, Category, CreateDate FROM Events ORDER BY EventID DESC LIMIT " + num +", 10"; 
-    var objson = [];
-    db.query(Q, function (err, result, fields) {
-        if (err) throw err;
-        for (var i = 0 ; i < result.length;i++) {
-            objson.push({
-                EventID:result[i].EventID,
-                EventName:result[i].EventName, Category:result[i].Category, CreateDate:result[i].CreateDate});
-        }
-        res.send(JSON.stringify(objson));
-    });
+        var skip = req.body.pageNum;
+        var num = parseInt(skip)*10;
+        
+        var db = req.con;
+        //db.connect(function(err){if (err) throw err;});
+        var objson = [];
+        db.query("SELECT EventID, EventName, Category, CreateDate FROM Events ORDER BY EventID DESC LIMIT ?, 10", [num], function (err, result, fields) {
+            if (err) throw err;
+            console.log(result[0]);
+            for (var i = 0 ; i < result.length;i++) {
+                objson.push({
+                    EventID:result[i].EventID,
+                    EventName:result[i].EventName, Category:result[i].Category, CreateDate:result[i].CreateDate});
+            }
+            res.send(JSON.stringify(objson));
+        });
     })
 
     .post('/projects_ajax', function(req, res, next) {
-    var skip = req.body.pageNum;
-    var num = parseInt(skip)*10;
-    
-    var db = req.con;
-    //db.connect(function(err){if (err) throw err;});
-    var Q = "SELECT ProjectID, LabName, Description FROM Projects ORDER BY ProjectID DESC LIMIT " + num +", 10"; 
-    var objson = [];
-    db.query(Q, function (err, result, fields) {
-        if (err) throw err;
-        for (var i = 0 ; i < result.length;i++) {
-            objson.push({
-                ProjectID:result[i].ProjectID, LabName:result[i].LabName, Description:result[i].Description});
-        }
-        res.send(JSON.stringify(objson));
-    });
+        var skip = req.body.pageNum;
+        var num = parseInt(skip)*10;
+        
+        var db = req.con;
+        //db.connect(function(err){if (err) throw err;});
+        var objson = [];
+        db.query("SELECT ProjectID, LabName, Description FROM Projects ORDER BY ProjectID DESC LIMIT ?, 10", [num], function (err, result, fields) {
+            if (err) throw err;
+            for (var i = 0 ; i < result.length;i++) {
+                objson.push({
+                    ProjectID:result[i].ProjectID, LabName:result[i].LabName, Description:result[i].Description});
+            }
+            res.send(JSON.stringify(objson));
+        });
     })
 
 
